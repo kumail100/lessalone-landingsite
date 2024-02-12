1aab68219747b7607825892710998d9155f8e17b
Kumail Akbari <kumailakbari@gmail.com>
2024-02-12 17:12:45 -0500

    removing staging folder adding firebase tools

--- website/gatsby-config.ts
+++ website/gatsby-config.ts
@@ -1,5 +1,26 @@
 import type { GatsbyConfig, PluginRef } from "gatsby"
 import "dotenv/config"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB6AIauVAJjgPSYUcmcQmjcFS5ZCq3A2qg",
+  authDomain: "lessalone-bd53f.firebaseapp.com",
+  projectId: "lessalone-bd53f",
+  storageBucket: "lessalone-bd53f.appspot.com",
+  messagingSenderId: "799491192801",
+  appId: "1:799491192801:web:9a4183a09eb598ba30c4a0",
+  measurementId: "G-8QY1FQJY8S"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
